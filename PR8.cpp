b20442cc499602c39910b97f4724e2dc231b47d8
ttw1St0fF4te <isip_e.e.novikov@mpt.ru>
2024-06-07 14:37:16 +0300

    1

---
+++ PR8.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+
+
+int main()
+{
+    std::string str, symbols;
+    std::cout << "Enter the string: ";
+    std::cin >> str;
+    std::cout << "Enter the symbols to find: ";
+    std::cin >> symbols;
+
+    HMODULE library = LoadLibrary(L"Liba.dll");
+
+    typedef bool (*func) (std::string& str, std::string& symbols);
+    func Func;
+
+    Func = (func)GetProcAddress(library, "find_symbols");
+
+    bool result = Func(str, symbols);
+    std::cout << (result ? "All symbols are found in the string." : "Not all symbols are found in the string.") << std::endl;
+
+    FreeLibrary(library);
+    return 0;
+}
